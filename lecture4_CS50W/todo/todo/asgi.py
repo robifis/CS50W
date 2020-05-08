45a3d8d17fcef6739ae0750d590f95771d880ea3
robifis <bobby@olejnik.dev>
2020-05-08 14:34:15 +0100

    Starting Lecture 4

--- lecture4_CS50W/todo/todo/asgi.py
+++ lecture4_CS50W/todo/todo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cs50 project.
+ASGI config for todo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs50.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
 application = get_asgi_application()
--- lecture4_CS50W/todo/todo/urls.py
+++ lecture4_CS50W/todo/todo/urls.py
@@ -1,5 +1,4 @@
-"""
-cs50 URL Configuration
+"""todo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')) 
 ]
-
-"""
-This tells the django project that if we type url.com/hello into the address bar that
-we want it to include everything inside of the urls.py file that's inside of that file.
-"""
